7a91a6d796b11552631a79ef3ba7b9791af290e3
Mehrab <mehrab.kor@gmail.com>
2023-01-30 17:40:04 +0330

    springSecurity demo & authentication

---
+++ src/main/java/com/mehrab/springdemo/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.mehrab.springdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+// SecurityContextHolder.class ->  public static SecurityContext getContext(){...}
+// SecurityContext -> is Holder that has the Authentication inside. this is only true in the servlet world and alive in the thread(anything in thread that process on request can access to this security context (inside controller))
+// Thread-local, not propagated to child  threads - cleared after requests is 100% processed
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests((authz) -> {
+                        authz.requestMatchers("/").permitAll();
+                        authz.requestMatchers("/error").permitAll();
+                        authz.requestMatchers("/favicon.ico").permitAll();
+                        authz.anyRequest().authenticated();
+                }).formLogin(Customizer.withDefaults()) // set springboot default login page for forbidden pages
+//                .oauth2Login(Customizer.withDefaults()) // we tell filterchain that login with openID (built on top of oauth2)
+                .build();
+
+    }
+
+
+    // UserDetailsService -> repository of users
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(User.builder()
+                .username("mehrab")
+                .password("{noop}zaq123")
+                .authorities("ROLE_user")
+                .build());
+    }
+}
---
+++ src/main/java/com/mehrab/springdemo/controller/SecurityDemoController.java
@@ -0,0 +1,53 @@
+package com.mehrab.springdemo.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+// authentication.getPrincipal() or authentication.getDetails() returned Object so it can be extendable of rich of properties
+// securityContextHolder is a thread local variable that means its only available in the thread thats processing the request.
+// if i spawn more thread that going to talk to database or do more calls they do not have access to the security context.
+// securityContext cleaned after the request is processed then can be used for other next requests(doesnt steal authentication)
+
+@RestController
+public class SecurityDemoController {
+
+    @RequestMapping(method = RequestMethod.GET, path = "/public")
+    public String publicPage() {
+        return "hello mehrab";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/private")
+    public String privatePage(Authentication authentication) throws InterruptedException {
+        // way2(injecting) -> same as authentication
+        Authentication authentication1 = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("OUTSIDE THREAD -> " + authentication1);
+
+        Thread thread = new Thread(() -> {
+            Authentication authentication2 = SecurityContextHolder.getContext().getAuthentication();
+            System.out.println("IN THREAD -> " + authentication2);
+        });
+        thread.start();
+        thread.join();
+
+        System.out.println(authentication.getDetails());
+
+        return "this page shoulde be private😍😍 \n welcome to the VIP room ~[" + getName(authentication) + "]~";
+    }
+    
+    public static String getName(Authentication authentication) {
+        // if login with OID get the user email or get name
+        return Optional.of(authentication.getPrincipal())
+                .filter(OidcUser.class::isInstance)
+                .map(OidcUser.class::cast)
+                .map(OidcUser::getEmail)
+                .orElseGet(authentication::getName);
+
+    }
+}
